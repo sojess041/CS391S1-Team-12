b87387c25b9cd56f7f3b570085175427baee0c5d
Jonathan Singh <jonathan@mac.mynetworksettings.com>
2025-11-28 19:52:56 -0500

    Implement signup/login backend

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -32,7 +32,7 @@ export default function LoginPage() {
     }
 
     alert("Logged in successfully!");
-    window.location.href = "/dashboard"; // redirect after login
+    window.location.href = "/"; // redirect after login
   };
 
   const signInWithGoogle = async () => {
