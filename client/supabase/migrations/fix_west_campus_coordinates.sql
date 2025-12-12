34766ab4355b453ab79585db596b3143ef16f828
ithlambelle <lambelle@bu.edu>
2025-12-11 22:21:40 -0500

    updates

--- client/supabase/migrations/fix_west_campus_coordinates.sql
+++
@@ -1,6 +0,0 @@
--- Fix West Campus coordinates
--- Update The Fresh Food Co. at West Campus to correct coordinates
-UPDATE public.locations
-SET lat = 42.3505, lng = -71.1025
-WHERE slug = 'west-campus' OR name LIKE '%West Campus%';
-
